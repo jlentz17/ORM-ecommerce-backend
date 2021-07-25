355eff0b0b73513aa06a973ebf3845ba85599525
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-24 19:51:03 -0700

    added some of the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,6 +1,45 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { Category } = require("../../models");
 
-// GET /api/
+// GET /api/category
+router.get("/", (req, res) => {
+    Category.findAll({
+
+    })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
+// GET /api/category/1
+router.get("/:id", (req, res) => {
+
+})
+.then(dbCategoryData => {
+    if (!dbCategoryData) {
+        res.json(dbCategoryData);
+    })  
+        .catch(err) {
+            res.status(404).json({ message: "No category found" });
+        }
+    }
+})
+
+// POST /api/category
+router.post("/", (req, res) => {
+
+})
+
+// PUT /api/category/1
+router.put("/:id", (req, res) => {
+
+})
+
+// DELETE /api/category/1
+router.delete("/:id", (req, res) => {
+
+})
 
 module.exports = router;
