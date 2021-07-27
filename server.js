24ca1c2b4c86d7208a0b787378a9d5d6e83c4acd
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-27 13:12:17 -0700

    test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const { sequelize } = require(".models/Category.js")
 const routes = require("./routes");
-const connectiondb = require("./config/connection");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
